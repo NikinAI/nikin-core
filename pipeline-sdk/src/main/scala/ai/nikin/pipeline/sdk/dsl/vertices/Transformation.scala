53f1c71fe83838256c38c0b4d6d55be4c913d007
Jorge Aliss <jaliss@gmail.com>
2022-11-29 16:13:44 -0300

    added AggregationFunction

--- pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/dsl/vertices/Transformation.scala
+++ pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/dsl/vertices/Transformation.scala
@@ -1,9 +1,23 @@
 package ai.nikin.pipeline.sdk.dsl.vertices
 
+import ai.nikin.pipeline.sdk.dsl.vertices.Aggregation.AggregationFunction
 import ai.nikin.typedgraph.core.Vertex
 
-case class Transformation[_IN, _OUT](n: String)
-    extends Vertex[Transformation[_IN, _OUT]](s"transformation-$n") {
+abstract class Transformation[_IN, _OUT](n: String) extends Vertex[Transformation[_IN, _OUT]](n) {
   override type IN  = _IN
   override type OUT = _OUT
+}
+
+case class Aggregation[_IN, _OUT](n: String, fn: AggregationFunction)
+    extends Transformation[_IN, _OUT](n)
+
+object Aggregation {
+  sealed trait AggregationFunction {
+    def col: String
+  }
+  case class Avg(col: String) extends AggregationFunction
+  case class Max(col: String) extends AggregationFunction
+  case class Min(col: String) extends AggregationFunction
+  case class Sum(col: String) extends AggregationFunction
+  case class Count(col: String) extends AggregationFunction
 }
