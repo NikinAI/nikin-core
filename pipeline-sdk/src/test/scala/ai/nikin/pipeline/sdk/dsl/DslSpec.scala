0c4c7b5ceb33c0d9ca4ca3a49cb9eb87d64b98ed
Jorge Aliss <jaliss@gmail.com>
2022-11-29 16:13:44 -0300

    added AggregationFunction

--- pipeline-sdk/src/test/scala/ai/nikin/pipeline/sdk/dsl/DslSpec.scala
+++ pipeline-sdk/src/test/scala/ai/nikin/pipeline/sdk/dsl/DslSpec.scala
@@ -3,20 +3,21 @@ package ai.nikin.pipeline.sdk.dsl
 import ai.nikin.pipeline.sdk.TestUtils.testContained
 import ai.nikin.pipeline.sdk.dsl.edge.{lakeToTransform, transformToLake, F_>>>}
 import ai.nikin.pipeline.sdk.dsl.model.{RecordA, RecordB}
-import ai.nikin.pipeline.sdk.dsl.vertices.{lake, transformation, Lake, Transformation}
+import ai.nikin.pipeline.sdk.dsl.vertices.Aggregation.{Avg, Sum}
+import ai.nikin.pipeline.sdk.dsl.vertices.{aggregation, lake, Aggregation, Lake, Transformation}
 import ai.nikin.typedgraph.core._
 import munit.FunSuite
 
-class GraphSpec extends FunSuite {
-  test("Graph - simple edge") {
+class DslSpec extends FunSuite {
+  test("DSL - aggregation to lake") {
     val pipeline: Transformation[RecordA, RecordB] F_>>> Lake[RecordB] =
-      transformation[RecordA, RecordB]("s1") >>> lake[RecordB]("t1")
+      aggregation[RecordA, RecordB]("s1", Sum("col1")) >>> lake[RecordB]("t1")
 
     println(pipeline)
 
     testContained(
       compileErrors(
-        """ transformation[RecordB, RecordA]("s1") >>> lake[RecordB]("t1") """
+        """ aggregation[RecordB, RecordA]("s1", Sum("col1")) >>> lake[RecordB]("t1") """
       ),
       "inferred type arguments",
       "do not conform to method",
@@ -31,9 +32,10 @@ class GraphSpec extends FunSuite {
     )
   }
 
-  test("Graph - lake to transform to lake") {
-    val pipeline: Lake[RecordA] >>> Lake[RecordB] =
-      lake[RecordA]("lA") >>> transformation[RecordA, RecordB]("tAB") >>> lake[RecordB]("lB")
+  test("DSL - lake to aggregation to lake") {
+    val pipeline =
+      lake[RecordA]("lA") >>> aggregation[RecordA, RecordB]("tAB", Avg("col1")) >>>
+        lake[RecordB]("lB")
 
     println(pipeline.inGraph.toDot)
     println(pipeline)
