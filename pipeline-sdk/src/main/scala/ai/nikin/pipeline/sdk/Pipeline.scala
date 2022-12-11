b6d5503aee36a652e41802d4d098f838877409c2
Fran Bermejo <francisco.bermejo84@gmail.com>
2022-12-11 13:42:23 +0100

    Run scalafmt

--- pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/Pipeline.scala
+++ pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/Pipeline.scala
@@ -1,12 +1,14 @@
 package ai.nikin.pipeline.sdk
 
-import ai.nikin.typedgraph.core.{Graph, Vertex, Path}
+import ai.nikin.typedgraph.core.{Graph, Path, Vertex}
 
 import scala.language.implicitConversions
 
 trait Pipeline {
   type PipelineDef = Graph
-  implicit def toGraph[FROM <: Vertex[FROM], TO <: Vertex[TO]](tg: Path[FROM, TO]): PipelineDef = tg.asGraph
+
+  implicit def toGraph[FROM <: Vertex[FROM], TO <: Vertex[TO]](tg: Path[FROM, TO]): PipelineDef =
+    tg.asGraph
 
   def definition: PipelineDef
 }
