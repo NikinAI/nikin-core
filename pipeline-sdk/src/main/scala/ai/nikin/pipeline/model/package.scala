5e2b610a7054b02171da7c14730259dcb644879f
Fran Bermejo <francisco.bermejo84@gmail.com>
2023-01-18 13:02:01 +0100

    Untyped

--- pipeline-sdk/src/main/scala/ai/nikin/pipeline/model/package.scala
+++ pipeline-sdk/src/main/scala/ai/nikin/pipeline/model/package.scala
@@ -21,7 +21,7 @@ package object model {
   }
 
   object Typeless {
-    sealed trait Vertex
+    sealed trait Vertex extends BaseVertex
     case class Lake(name: String, tpe: String) extends BaseLake with Vertex
 
     case class Aggregation(
@@ -32,12 +32,26 @@ package object model {
     ) extends BaseAggregation
         with Vertex
 
-//    import scalax.collection.edges.{DiEdge, DiEdgeImplicits}
-//    import scalax.collection.immutable.{Graph, TypedGraphFactory}
-//
-//    type PipelineDef = Graph[Vertex, DiEdge[Vertex]]
-//
-//    object PipelineDef extends TypedGraphFactory[Vertex, DiEdge[Vertex]]
+    import scalax.collection.edges.{DiEdge, DiEdgeImplicits}
+    import scalax.collection.immutable.{Graph, TypedGraphFactory}
+
+    def addVertex[V <: Vertex](v: V, graph: PipelineDef): PipelineDef = graph + v
+
+    def removeVertex[V <: Vertex](v: V, graph: PipelineDef): PipelineDef = graph - v
+
+    def addEdge[V1 <: Vertex, V2 <: Vertex](v1: V1, v2: V2, graph: PipelineDef): PipelineDef =
+      (v1, v2) match {
+        case (l: Lake, agg: Aggregation) if l.tpe == agg.inputTpe  => graph + (l ~> agg)
+        case (agg: Aggregation, l: Lake) if agg.outputTpe == l.tpe => graph + (agg ~> l)
+        case _                                                     => throw new RuntimeException("...")
+      }
+
+    def removeEdge[V1 <: Vertex, V2 <: Vertex](v1: V1, v2: V2, graph: PipelineDef): PipelineDef =
+      graph - (v1 ~> v2)
+
+    type PipelineDef = Graph[Vertex, DiEdge[Vertex]]
+
+    object PipelineDef extends TypedGraphFactory[Vertex, DiEdge[Vertex]]
 
   }
 
