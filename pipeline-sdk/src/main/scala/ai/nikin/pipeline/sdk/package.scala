47cf2efb1eee7e35f941aac7ea3a3db7b46b418a
Fran Bermejo <francisco.bermejo84@gmail.com>
2023-02-16 11:35:25 +0100

    Improvements

--- pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/package.scala
+++ pipeline-sdk/src/main/scala/ai/nikin/pipeline/sdk/package.scala
@@ -19,21 +19,16 @@ package object sdk {
   implicit def toGraph[V <: Vertex[V]](pb: PipelineBuilder[V]): PipelineDef = pb.graph
 
   implicit def toPipelineBuilder[V <: Vertex[V]](v: V): PipelineBuilder[V] =
-    PipelineBuilder(v, Graph.empty[Vertex[_], DiEdge])
+    new PipelineBuilder(v, Graph.empty[Vertex[_], DiEdge])
 
-  class PipelineBuilder[SELF <: Vertex[SELF]](
+  class PipelineBuilder[SELF <: Vertex[SELF]] private[sdk] (
       private[sdk] val v:     SELF,
       private[sdk] val graph: PipelineDef
   ) {
     def >>>[
         V <: VertexTO[SELF, V]
     ](next: V)(implicit @unused ev: CanMakeEdge[SELF, V]): PipelineBuilder[V] =
-      PipelineBuilder(next, graph ++ Set(v ~> next))
-  }
-
-  private object PipelineBuilder {
-    def apply[V <: Vertex[V]](v: V, graph: PipelineDef): PipelineBuilder[V] =
-      new PipelineBuilder(v, graph)
+      new PipelineBuilder(next, graph ++ Set(v ~> next))
   }
 
   type VertexTO[FROM <: Vertex[FROM], TO <: Vertex[TO] { type IN = FROM#OUT }] =
